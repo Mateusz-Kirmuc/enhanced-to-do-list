cbe1b67440a9fcf839f2eff0395517982a8b49c7
mateusz <mateusz@kirmuc.pl>
2018-01-04 20:15:45 +0100

    Create minimal working list-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListDetailsComponent } from './list-details/list-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListDetailsComponent
   ],
   imports: [
     BrowserModule
--- src/app/list-details/list-details.component.ts
+++ src/app/list-details/list-details.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-details.component.css']
 })
 export class ListDetailsComponent implements OnInit {
-
+  tasks: string[] = [];
   constructor() { }
 
   ngOnInit() {
   }
 
+  addTask(task: string) {
+    task.trim();
+    if (!task) { return; }
+    this.tasks.push(task);
+  }
+
+  deleteTask(task: string) {
+    let index = this.tasks.indexOf(task);
+    this.tasks.splice(index, 1);
+  }
 }
