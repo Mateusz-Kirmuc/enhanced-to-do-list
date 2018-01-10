8908bce6acd236b15d272667e71ab0972d13241d
mateusz <mateusz@kirmuc.pl>
2018-01-10 10:28:32 +0100

    Implement httpclient into getLists and getList

--- src/app/list.service.ts
+++ src/app/list.service.ts
@@ -2,18 +2,42 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { List } from './list';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class ListService {
   lists: List[] = [];
+  private listsUrl = 'api/lists';
+
+  constructor(private http: HttpClient) { }
 
   getLists(): Observable<List[]> {
-    return of(this.lists);
+    return this.http.get<List[]>(this.listsUrl)
+      .pipe(catchError(this.handleError('getLists', [])))
   }
 
   getList(id: number): Observable<List> {
-    return of(this.lists.find(list => list.id === id))
+    const url = `${this.listsUrl}/${id}`;
+    return this.http.get<List>(url).pipe(
+      catchError(this.handleError<List>(`getHero id=${id}`))
+    );
   }
 
-  constructor() { }
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
